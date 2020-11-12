45fe37f38fea0fef3bf88a232910b6c09cfc276e
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-11-12 17:33:46 +0530

    [Abhishek] added to the UC23 branch

--- RestSharpTest.cs
+++ RestSharpTest.cs
@@ -22,6 +22,15 @@ namespace AddressBookJsonServer
             public string state { get; set; }
             public string phone { get; set; }
             public string email { get; set; }
+
+            public AddressBook(string name, string city, string state, string phone, string email)
+            {
+                this.name = name;
+                this.city = city;
+                this.state = state;
+                this.phone = phone;
+                this.email = email;
+            }
         }
         public IRestResponse GetAddressBook()
         {
@@ -45,6 +54,33 @@ namespace AddressBookJsonServer
                 Console.WriteLine("city: "+addressData.city);
             }
             );
+        }
+        /// Adding a list of
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void OnCallingPostAddsMultipleAddress() {
+            List<AddressBook> addressBook = new List<AddressBook>();
+            addressBook.Add(new AddressBook("Mahesh","Kochi","Kerala","8993839","mahesh@gmail.com"));
+            addressBook.Add(new AddressBook("Prasoon", "Mathura", "UP", "837731", "prasoon@gmail.com"));
+            addressBook.Add(new AddressBook("Ketaki", "Salem", "TN", "7383939", "ketaki@gmail.com"));
+            List<AddressBook> result = new List<AddressBook>();
+            addressBook.ForEach(address => {
+                RestRequest request = new RestRequest(" /AddressBook/list", Method.POST);
+                JsonObject jsonObj = new JsonObject();
+                jsonObj.Add("name", address.name);
+                jsonObj.Add("city", address.city);
+                jsonObj.Add("state", address.state);
+                jsonObj.Add("phone", address.phone);
+                jsonObj.Add("email", address.email);
+                request.AddParameter("application/json", jsonObj, ParameterType.RequestBody);
+                IRestResponse response = client.Execute(request);
+                Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+                AddressBook book = JsonConvert.DeserializeObject<AddressBook>(response.Content);
+                result.Add(book);
+            }
+            );
+            Assert.AreEqual(3, result.Count);
         }
     }
 }
